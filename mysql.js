7fcd83f488129063713a213d84e965a898922033
Matheus Guimaraes <matheusguimaraes668@gmail.com>
2024-10-23 09:50:31 -0300

    Adicionando variáveis de ambiemte

--- mysql.js
+++ mysql.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 let pool = mysql.createPool({
-  user: 'root',
-  password: 'root',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'ecommerce',
-  host: 'localhost',
-  port: 3307,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 });
 
 exports.pool = pool;

6e267b7356a1628a702e499cf76b4e9b26dd716f
Matheus Guimaraes <matheusguimaraes668@gmail.com>
2024-10-23 10:06:28 -0300

    feito o .env

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 let pool = mysql.createPool({
   user: 'Matheus',
-  password: 'AVNS_Tmk7RqYEHT6pq6V7YuZ',
+  password: process.env.DB_PASS,
   database: 'defaultdb',
   host: 'db-mysql-nyc3-88566-do-user-17142564-0.d.db.ondigitalocean.com',
   port: 25060,
